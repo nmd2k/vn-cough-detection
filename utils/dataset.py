698b1dcef6e01ab79bfa40a3395a878b46b40b9a
manhdung20112000 <manhdung20112000@gmail.com>
2021-06-26 10:28:57 +0700

    custom dataset

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,5 +1,8 @@
-from logging import root
+import os
 import torch
+import pandas as pd
+from PIL import Image
+from torchvision import transforms
 from torch.utils.data import Dataset
 
 class AICoughDataset(Dataset):
@@ -15,18 +18,37 @@ class AICoughDataset(Dataset):
     def __init__(self, root_path, is_train=True, transform=None):
         self.path      = root_path
         self.train     = is_train
-        self.transform = transform
+        if transform == None:
+            trans = transforms.ToTensor()
+        else:
+            self.transform = transform
 
-        pass
+        if is_train:
+            csv = pd.read_csv(os.path.join(root_path, 'train', '*.csv'))
+        else:
+            csv = pd.read_csv(os.path.join(root_path, 'test', '*.csv'))
+
+        self.ids       = csv['uuid']
+        self.target    = csv['assessment_result']
     
     def __len__(self):
-        pass
+        return len(self.ids)
 
     def __getitem__(self, index):
+        id = self.ids[index]
+
         if not self.train:
             # testset return id and input
-            pass
+            img = Image.open(os.path.join(self.path, 'test', id + '.jpg'))
+            img = self.transform(img)
+            return id, img
         
         # trainset return input and target
-        return None
+        img    = Image.open(os.path.join(self.path, 'train', id + '.jpg'))
+        target = float(self.target[id])
+
+        img    = self.transform(img)
+        target = self.transform(target)
+
+        return img, target
         
