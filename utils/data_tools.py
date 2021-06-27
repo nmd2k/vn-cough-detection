16bebaf8cfd7b077c7d523cfd3dc936c0f90ab56
manhdung20112000 <manhdung20112000@gmail.com>
2021-06-27 11:05:32 +0700

    merge fix-length-dataset branch

---
+++ utils/data_tools.py
@@ -0,0 +1,81 @@
+import torch
+import random
+import torchaudio
+import torchaudio.functional as F
+import torchaudio.transforms as T
+
+class AudioUtil():
+    def rechannel(aud, new_channel):
+        """
+        Convert the given audio to the desired number of channels
+        """
+        sig, sr = aud
+
+        if (sig.shape[0] == new_channel):
+            # Nothing to do
+            return aud
+
+        if (new_channel == 1):
+            # Convert from stereo to mono by selecting only the first channel
+            resig = sig[:1, :]
+        else:
+            # Convert from mono to stereo by duplicating the first channel
+            resig = torch.cat([sig, sig])
+
+        return ((resig, sr))
+
+    def resample(aud, newsr):
+        """
+        Resample the given audio to have original sr = newsr
+        """
+        sig, sr = aud
+
+        if (sr == newsr):
+             # Nothing to do
+            return aud
+
+        num_channels = sig.shape[0]
+            # Resample first channel
+        resig = torchaudio.transforms.Resample(sr, newsr)(sig[:1,:])
+        if (num_channels > 1):
+            # Resample the second channel and merge both channels
+            retwo = torchaudio.transforms.Resample(sr, newsr)(sig[1:,:])
+            resig = torch.cat([resig, retwo])
+
+        return ((resig, newsr))
+
+    def pad_trunc(aud, max_ms):
+        """
+        Pad or truncate a given audio to have length in miliseconds = max_ms
+        """
+        signal, sr = aud
+        num_rows, sig_len = signal.shape
+        max_len = int(sr/1000 * max_ms)
+        if (sig_len > max_len):
+            #Truncate the signal 
+            signal = signal[:, :max_len]
+        elif (sig_len < max_len):
+            #Length of padding to add at the begin and end of the signal
+            pad_begin_len = random.randint(0, max_len - sig_len)
+            pad_end_len = int(max_len - sig_len - pad_begin_len)
+
+            #Pad 0s
+            begin_sig = torch.zeros(num_rows, pad_begin_len)
+            end_sig = torch.zeros(num_rows, pad_end_len)
+            signal = torch.cat([begin_sig, signal, end_sig], dim=1)
+        return (signal, sr)
+
+    def get_spectrogram(aud, n_mels=64, n_fft=1024, hop_len=None):
+        signal,sr = aud
+        # spec has shape [channel, n_mels, time], where channel is mono, stereo etc
+        get_spec =  T.MelSpectrogram(sr, n_fft=n_fft, hop_length=hop_len, n_mels=n_mels)
+        spec = get_spec(signal)
+        #print(spec.shape)
+        return spec
+
+    def get_mfcc(aud):
+        signal, sr = aud
+        get_mfcc_func = T.MFCC(sample_rate = sr)
+        mfcc = get_mfcc_func(signal)
+        #print(mfcc.shape)
+        return mfcc 
--- utils/dataset.py
+++ utils/dataset.py
@@ -1,11 +1,11 @@
 import os
 import torch
+import torchaudio
 import pandas as pd
 from PIL import Image
 from torchvision import transforms
 from torch.utils.data import Dataset
-import utils.Audioutils as utils
-
+from utils.data_tools import AudioUtil
 
 class AICoughDataset(Dataset):
     """
@@ -21,7 +21,7 @@ class AICoughDataset(Dataset):
         self.path      = root_path
         self.train     = is_train
         if transform == None:
-            trans = transforms.ToTensor()
+            self.transform = transforms.ToTensor()
         else:
             self.transform = transform
 
@@ -54,3 +54,48 @@ class AICoughDataset(Dataset):
 
         return img, target
         
+class FixedLenDataset(Dataset):
+    def __init__(self, root_path, mfcc=True, is_train=True, transform=None):
+        """
+        Fixed length cough covid dataset
+        Args:
+            root_path (string): Path to dataset directory
+            mfcc (bool): Return MFCC image if True, otherwise Mel spectrogram
+            is_train (bool): Train dataset or test dataset
+            transform (function): whether to apply the data augmentation scheme
+                    mentioned in the paper. Only applied on the train split.
+        """
+
+        self.root = root_path
+        self.mfcc = mfcc
+
+        if is_train:
+            self.df = pd.read_csv(os.path.join(root_path, 'metadata_train_challenge.csv'))
+        else: 
+            self.df = pd.read_csv(os.path.join(root_path, 'metadata_test_challenge.csv'))
+
+        if transform == None:
+            self.transform = transforms.ToTensor()
+        else:
+            self.transform = transform
+
+    
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        aud   = torchaudio.load(self.root + self.df.loc[idx, 'file_path'])
+        label = self.df.loc[idx, 'assessment_result']
+        aud   = AudioUtil.pad_trunc(aud, max_ms = 10000)
+        spec  = AudioUtil.get_spectrogram(aud, n_mels=64, n_fft=1024, hop_len=None)
+        mfcc  = AudioUtil.get_mfcc(aud)
+
+        # transform
+        label = self.transform(label)
+        mfcc  = self.transform(mfcc)
+        spec  = self.transform(spec)
+
+        if self.mfcc:
+            return mfcc, label
+
+        return spec, label
