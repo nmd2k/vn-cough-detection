40222e380d7ffb95df379c429bbc5e107ffc8fcf
hoangngx <50651535+hoangngx@users.noreply.github.com>
2021-06-30 23:46:38 +0700

    Add files via upload
    postprocess after timestamped

---
+++ postprocess.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+
+sample = "C:\\Users\\hoang\\Desktop\\vn-cough-detection-main\\private_test_sample_submissions.csv"
+output = "C:\\Users\\hoang\\Desktop\\vn-cough-detection-main\\results.csv"
+
+sample_df = pd.read_csv(sample)
+
+df = pd.read_csv(output)
+df['uuid'] = df['uuid'].str[:36]
+
+
+aggregation_functions = {'assessment_result': 'mean'}
+df_new = df.groupby(df['uuid']).aggregate(aggregation_functions)
+
+# df_new.info()
+
+final_df = pd.merge(sample_df, df_new, on='uuid', how='left')
+del final_df["assessment_result_x"]
+final_df.columns = ['uuid', 'assessment_result']
+
+final_df['assessment_result'] = final_df['assessment_result'].fillna(0)
+
+print(final_df.head())
+
+final_df.to_csv('results.csv')
